681a178625a0accbfc06a77a73a4475c7bfea440
naoki <n.globe.us@gmail.com>
2019-03-06 01:55:35 +0900

    First commit

---
+++ do-not-share-post-media.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:     Do Not Share Post Media
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          Naoki Ohashi
+ * Author URI:      https://naoki-is.me/
+ * Text Domain:     do-not-share-post-media
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Do_Not_Share_Post_Media
+ */
+
---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Do_Not_Share_Post_Media
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
